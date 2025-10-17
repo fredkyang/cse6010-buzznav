d6a2ffe080229d624fdc71f4e50a971c652182ae
Fred Yang <fred.yang@gatech.edu>
2025-10-17 04:32:11 -0400

    Update main.c to integrate graph module

--- src/main.c
+++ src/main.c
@@ -2,12 +2,9 @@
 #include <stdlib.h>
 #include "graph.h"
 
-
-
-
 int main(int argc, char *argv[]){
-    if (argc < 3) {
-        printf("Usage: %s <start building name> <goal building name>\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "\033[33m:( Usage: %s \"<start building name>\" \"<goal building name>\"\n     e.g. %s \"Student Center\" \"College of Computing Building\"\n", argv[0], argv[0]);
         return 1;
     }
 
@@ -15,27 +12,29 @@ int main(int argc, char *argv[]){
     const char* goal_building = argv[2];
 
     // Step 1: Load graph from file
-    Graph* campus = load_graph("../data/adj_list.csv"); // Build adjacency list of the campus
+    Graph* campus = load_graph("./data/adj_list.csv"); // Build adjacency list of the campus
     if (!campus) {
-        fprintf(stderr, "Failed to load adj_list.csv file.\n");
         return 1;
     }
 
-    BuildingMapping* mapping = load_building("../data/building_mapping.csv"); // Load building mapping
+    // print graph to check its structure
+    // print_graph(campus);
+
+    BuildingMapping* mapping = load_building("./data/building_mapping.csv"); // Load building mapping
     if (!mapping) {
-        fprintf(stderr, "Failed to load building_mapping.csv file.\n");
         return 1;
     }
 
     // Step 2: Search building node IDs
-    const char* start_building = argv[1];
-    const char* goal_building = argv[2];
-    int start_id = get_node_id(mapping, start_building);
-    int goal_id = get_node_id(mapping, goal_building);
-    free_building_mapping(mapping); // free the building mapping
+    int start_id = get_building_id(mapping, start_building);
+    int goal_id = get_building_id(mapping, goal_building);
+    free_building(mapping); // free the building mapping
 
-    printf("Start node ID: %d\n", start_id);
-    printf("Goal node ID: %d\n", goal_id);
+    if (start_id == -1 || goal_id == -1) {
+        return 1;
+    }
+
+    
 
     // Step 3: Run algorithm
 
