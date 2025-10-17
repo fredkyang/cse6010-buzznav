84be9420cbd067bb03476a783825dfc1d887b60f
HEJINF <1098204483@qq.com>
2025-10-17 10:29:15 -0400

    Update astar.c with printf

--- src/astar.c
+++ src/astar.c
@@ -56,7 +56,7 @@ int find_min_node(AStarNode* nodes, int num_nodes)
         {   
             min_f = nodes[i].f_score;
             min_idx = i;
-            /* code */
+           
         }
         
     }
@@ -64,5 +64,65 @@ int find_min_node(AStarNode* nodes, int num_nodes)
 }
 
 double astar(int start, int goal) {
-    
+    //step 1 check everything ok at first
+    /*
+    I do not know whether need this one
+     if (!graph) {
+        printf("error: graph not initialized\n");
+        return -1;
+    }
+    */
+   // node Id is valid or not
+   if (start<0||start>=graph->num_nodes||goal<0||goal>=graph->num_nodes)
+   {
+     printf("error: invalid node ID (start=%d, goal=%d, max=%d)\n", 
+        start, goal, graph->num_nodes - 1);
+        return -1;
+    /* code */
+   }
+   int num_nodes=graph->num_nodes;
+
+   // step 2  memory allocation and intiallization
+   AStarNode* nodes = (AStarNode*)calloc(num_nodes, sizeof(AStarNode));
+   if (!nodes)
+   {
+    printf("error:memory allocation fail\n");
+    return -1;
+    /* code */
+   }
+   for (int i = 0; i < num_nodes; i++)
+   {nodes[i].f_score=DBL_MAX;
+    nodes[i].g_score=DBL_MAX;
+    nodes[i].parent=-1;
+    nodes[i].visited=false;
+    /* code */
+   }
+
+   //step 3 initialize start
+   nodes[start].f_score=0;
+   nodes[start].g_score=0;
+   
+   //step 4
+/*
+*
+Since we already computed distances between adjacent buildings from the adjacency list, 
+our A* effectively behaves like Dijkstra’s algorithm. 
+However, we now have precise latitude–longitude coordinates for the nodes, 
+so if required we can compute edge costs (or a heuristic) directly from 
+geodesic distances using the Haversine function. 
+This lets us incorporate a proper heuristic and run true A* rather than pure Dijkstra.
+*
+*/
+// main a* loop
+/*
+* reach goal or all nodes
+*/
+
+while (1)
+{
+    /* code */
+}
+
+
+
 }
